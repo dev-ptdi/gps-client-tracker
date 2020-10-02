2c744d27b10c600cb1c7b5e8a2cc648fa3eee1b3
Tommy Khumarga <tommy@khumarga.com>
2020-10-02 18:18:00 +0700

    Dev

--- common.js
+++ common.js
@@ -37,32 +37,24 @@ $(function() {
         if(result.environment === 'development') _apiUrl = 'http://gps.bataviarent.com/prime/iot/v1/api/Traccar/Get_Token_InfoAsync';
         else if(result.environment === 'production') _apiUrl = 'http://localhost:5000/Traccar/Get_Token_InfoAsync';
 
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(pos) {
-                const coords = pos.coords;
+        _map = L.map('map').setView([0, 0], 13);
 
-                _map = L.map('map').setView([coords.latitude, coords.longitude], 13);
+        // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        //     maxZoom: 18,
+        //     id: 'mapbox/streets-v11',
+        //     tileSize: 512,
+        //     zoomOffset: -1,
+        //     accessToken: _mapBoxPublicToken,
+        // }).addTo(_map);
 
-                // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-                //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                //     maxZoom: 18,
-                //     id: 'mapbox/streets-v11',
-                //     tileSize: 512,
-                //     zoomOffset: -1,
-                //     accessToken: _mapBoxPublicToken,
-                // }).addTo(_map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(_map);
 
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(_map);
+        _markerGroup = L.layerGroup().addTo(_map);
 
-                _markerGroup = L.layerGroup().addTo(_map);
-
-                getLocations();
-            }, function(err) {
-                alert('Maps Error: ' + err);
-            });
-        } else alert('Please allow location request');
+        getLocations();
     });
 });
 
